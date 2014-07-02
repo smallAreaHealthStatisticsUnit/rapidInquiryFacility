e195156ec437b81fecbcd75ebb8799ac28ce277a
Kevin Garwood <kgarwood@imperial.ac.uk>
2014-07-02 16:51:57 +0100

    Removed references to XML annotations
    Removing import statements to various classes used to bind XML fields to
    Java objects.  We are now instead using parallel classes, where each
    major business class is associated with a class whose main job is to
    support serialisation with JSON

--- rifServices/src/main/java/rifServices/businessConceptLayer/AgeBand.java
+++ rifServices/src/main/java/rifServices/businessConceptLayer/AgeBand.java
@@ -11,14 +11,6 @@ import rifServices.util.FieldValidationUtility;
 import java.util.ArrayList;
 import java.text.Collator;
 
-
-import javax.xml.bind.annotation.XmlAttribute; 
-import javax.xml.bind.annotation.XmlAccessType; 
-import javax.xml.bind.annotation.XmlAccessorType; 
-import javax.xml.bind.annotation.XmlRootElement; 
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter; 
-
-
 /**
  *
  *
