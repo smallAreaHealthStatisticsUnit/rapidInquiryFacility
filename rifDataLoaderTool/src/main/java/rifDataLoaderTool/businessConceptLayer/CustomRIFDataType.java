56dbb34a25e3571702c271dc09888292b89e3350
Kevin Garwood <kgarwood@imperial.ac.uk>
2014-09-30 13:51:50 +0100

    Adding new data types to data loader

---
+++ rifDataLoaderTool/src/main/java/rifDataLoaderTool/businessConceptLayer/CustomRIFDataType.java
@@ -0,0 +1,138 @@
+package rifDataLoaderTool.businessConceptLayer;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ *
+ * <hr>
+ * Copyright 2014 Imperial College London, developed by the Small Area
+ * Health Statistics Unit. 
+ *
+ * <pre> 
+ * This file is part of the Rapid Inquiry Facility (RIF) project.
+ * RIF is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * RIF is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with RIF.  If not, see <http://www.gnu.org/licenses/>.
+ * </pre>
+ *
+ * <hr>
+ * Kevin Garwood
+ * @author kgarwood
+ */
+
+/*
+ * Code Road Map:
+ * --------------
+ * Code is organised into the following sections.  Wherever possible, 
+ * methods are classified based on an order of precedence described in 
+ * parentheses (..).  For example, if you're trying to find a method 
+ * 'getName(...)' that is both an interface method and an accessor 
+ * method, the order tells you it should appear under interface.
+ * 
+ * Order of 
+ * Precedence     Section
+ * ==========     ======
+ * (1)            Section Constants
+ * (2)            Section Properties
+ * (3)            Section Construction
+ * (7)            Section Accessors and Mutators
+ * (6)            Section Errors and Validation
+ * (5)            Section Interfaces
+ * (4)            Section Override
+ *
+ */
+
+public final class CustomRIFDataType 
+	implements RIFDataType {
+
+	// ==========================================
+	// Section Constants
+	// ==========================================
+
+	// ==========================================
+	// Section Properties
+	// ==========================================
+	private String name;
+	private String description;
+	private String validationRegularExpression;
+	
+	// ==========================================
+	// Section Construction
+	// ==========================================
+
+	public CustomRIFDataType() {
+
+	}
+
+	// ==========================================
+	// Section Accessors and Mutators
+	// ==========================================
+
+	public void setName(
+		final String name) {
+		
+		this.name = name;
+	}
+	
+	public String getName() {
+		
+		return name;
+	}
+	
+	public String getDescription() {
+		
+		return description;
+	}
+	
+	public void setDescription(
+		final String description) {
+		
+		this.description = description;
+	}
+	
+	public String getValidationRegularExpression() {
+		
+		return validationRegularExpression;
+	}
+	
+	public void setValidationRegularExpression(
+		final String validationRegularExpression) {
+		
+		this.validationRegularExpression = validationRegularExpression;
+	}
+
+	public boolean isValid(
+		final String value) {
+	
+		Pattern pattern = Pattern.compile(validationRegularExpression);
+		Matcher matcher = pattern.matcher(value);
+		return matcher.matches();
+	}
+	
+	
+	// ==========================================
+	// Section Errors and Validation
+	// ==========================================
+
+	// ==========================================
+	// Section Interfaces
+	// ==========================================
+
+	// ==========================================
+	// Section Override
+	// ==========================================
+
+}
+
+
---
+++ rifDataLoaderTool/src/main/java/rifDataLoaderTool/businessConceptLayer/RIFDataType.java
@@ -0,0 +1,42 @@
+package rifDataLoaderTool.businessConceptLayer;
+
+
+/**
+ *
+ *
+ * <hr>
+ * Copyright 2014 Imperial College London, developed by the Small Area
+ * Health Statistics Unit. 
+ *
+ * <pre> 
+ * This file is part of the Rapid Inquiry Facility (RIF) project.
+ * RIF is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * RIF is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with RIF.  If not, see <http://www.gnu.org/licenses/>.
+ * </pre>
+ *
+ * <hr>
+ * Kevin Garwood
+ * @author kgarwood
+ */
+
+
+public interface RIFDataType {
+	
+	public String getName();
+	public String getDescription();
+	public String getValidationRegularExpression();
+	public boolean isValid(final String value);
+
+}
+
+
