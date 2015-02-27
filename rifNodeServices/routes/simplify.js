1f2522b0df1dd91414aa3eb3c961a9ab8771c77e
fedeee <f.fabbri@imperial.ac.uk>
2015-02-27 18:13:00 +0000

    node simplify file handler
    Needs revision

--- rifNodeServices/routes/simplify.js
+++ rifNodeServices/routes/simplify.js
@@ -6,96 +6,24 @@
  *
  */
 
-var mapshaper = require('mapshaper'),
-    AdmZip = require('adm-zip');
-    fs = require('fs');
 
- 
-
-var shps = [],
-    directory = './output/',
-    outputDir = './output/',
-    zipName = '',
-    d = new Date();
-
-
-var copyFile = function(rd, target, cb,res) {
-  var cbCalled = false;
-  rd.on("error", function(err) {
-    done(err);
-  });
-  var wr = fs.createWriteStream(target);
-  wr.on("error", function(err) { 
-    done(err);
-  });
-  wr.on("close", function(ex) {
-    console.log('doneWriting')   
-    done();
-  });
-  rd.pipe(wr);
-
-  function done(err) {
-    if (!cbCalled) {
-      cb(err,res);
-      cbCalled = true;
-    }
-  }
-};
-
-
-var unzip = function(res){
-    var zip = new AdmZip(outputDir + zipName);
-    var zipEntries = zip.getEntries(); // an array of ZipEntry records
-    zipEntries.forEach(function(zipEntry) {
-        var fileName = zipEntry.name,
-            pathName = zipEntry.entryName;
-        if(fileName.split('.').pop() == 'shp'){
-            shps.push ([ pathName, fileName]);
-        };
-    });
-    try{
-        zip.extractAllTo( outputDir, true);
-        simplify(0);
-    }catch(e){
-        console.log('Exception occured' + e);
-    }
-};
-
-
-
-var simplify = function(i){
-    if( i >= shps.length){
-        return;
-    };
-    
-    console.log('-i ' + outputDir + shps[i][0]  +'  -simplify 5% -o ' + outputDir + 'simpl' + shps[i][1] );
-    var start = d.getTime();
-    mapshaper.runCommands('-i ' + outputDir + shps[i][0]  +'  -simplify 5% -o ' + outputDir + 'simpl' + shps[i][1] , function(error,dataset){
-       if(error) {
-         console.log(process.memoryUsage());    
-        throw   error;
-       }
-       dataset =null;    
-       console.log("done:" +  (d.getTime() - start) /1000 );
-       simplify(++i);    
-    });
-};
-
-
+var processData = require('./simplifyFilesHandler');  
 
 exports.convert = function(req, res) {
       res.setHeader("Content-Type", "text/plain");
+      
       if (req.method == 'POST') {
+         var dataProcessor = new processData.Process();
+         dataProcessor.makeOutFolder();
           
          req.busboy.on('file', function(fieldname, stream, filename, encoding, mimetype) {
-            zipName = filename;  
-            copyFile(stream, outputDir + filename, unzip,res);
+            dataProcessor.copyFile(stream, filename, res);
           });
            
          req.busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {});
           
          req.busboy.on('finish', function() {
-            console.log('finished');  
+           //addMsgToLog("Files are now processing....." ); 
            res.end('Files are now processing..'); 
          });
 
