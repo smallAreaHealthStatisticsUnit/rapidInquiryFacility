a11f8d750885ee83337ae85451d0530ecb2c4389
peterhambly <phambly@fastmail.co.uk>
2015-04-28 14:59:59 +0100

    Git ID test 5

--- rifDatabase/Postgres/PLpgsql/v4_0_rif40_xml_pkg.sql
+++ rifDatabase/Postgres/PLpgsql/v4_0_rif40_xml_pkg.sql
@@ -120,12 +120,12 @@ BEGIN
 -- Service information
 --
 	RETURN QUERY 
-		SELECT 'rif40_xml_pkg'::VARCHAR AS service_name, 
-			   'RIF XML/Jersey/JS support for middleware'::VARCHAR AS service_description,
-               '$Id$'::VARCHAR AS id,
-				4.0::NUMERIC AS version_number,
-				'Peter Hambly'::VARCHAR AS contact_name,
-				'p.hambly@imperial.ac.uk'::VARCHAR AS contact_email;
+		SELECT 'rif40_xml_pkg'::VARCHAR 							AS service_name, 
+			   'RIF XML/Jersey/JS support for middleware'::VARCHAR 	AS service_description,
+               '$Id$'::VARCHAR 										AS id,
+				4.0::NUMERIC 										AS version_number,
+				'Peter Hambly'::VARCHAR 							AS contact_name,
+				'p.hambly@imperial.ac.uk'::VARCHAR 					AS contact_email;
 --
 	RETURN;
 END;
