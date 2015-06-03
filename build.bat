42a1ddee2e17233761e5116116f3b4cd5e909f9c
peterhambly <phambly@fastmail.co.uk>
2015-06-03 09:52:45 +0100

    Windows build of rifServices using Maven OK

--- build.bat
+++ build.bat
@@ -1,1 +1,4 @@
-mvn --log-file build.log --errors --fail-at-end --file rifGenericLibrary --file rifServices clean validate compile package install war:war war:inplace
+powershell -ExecutionPolicy ByPass -file run.ps1 build.log . mvn.cmd --errors --fail-at-end --file rifGenericLibrary --file rifServices clean validate compile package install 
+if %errorlevel% neq 0 exit /b %errorlevel%
+powershell -ExecutionPolicy ByPass -file run.ps1 build.log rifServices mvn.cmd --errors --fail-at-end war:war war:inplace
+if %errorlevel% neq 0 exit /b %errorlevel%
