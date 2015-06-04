5428bf7c0af5cd83018bdc1f1c0fcc3d5e1b4d54
Kevin Garwood <kevin_garwood@hotmail.co.uk>
2015-06-04 09:10:41 +0100

    Changed target of script

--- build.bat
+++ build.bat
@@ -1,4 +1,2 @@
-powershell -ExecutionPolicy ByPass -file run.ps1 build.log . mvn.cmd --errors --fail-at-end --file rifGenericLibrary --file rifServices clean validate compile package install 
-if %errorlevel% neq 0 exit /b %errorlevel%
-powershell -ExecutionPolicy ByPass -file run.ps1 build.log rifServices mvn.cmd --errors --fail-at-end war:war war:inplace
+powershell -ExecutionPolicy ByPass -file run.ps1 build.log rapidInquiryFacility mvn.cmd --errors clean validate compile package install war:war war:inplace
 if %errorlevel% neq 0 exit /b %errorlevel%
