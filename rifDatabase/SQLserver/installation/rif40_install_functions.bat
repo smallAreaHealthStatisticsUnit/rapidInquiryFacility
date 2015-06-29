2223dcea5198cf091cd8b7fac2ec963d30403cec
Margaret Douglass <m.douglass@imperial.ac.uk>
2015-06-29 17:10:22 +0100

    SQL Server setup
    Add functions

---
+++ rifDatabase/SQLserver/installation/rif40_install_functions.bat
@@ -0,0 +1,7 @@
+sqlcmd -d sahsuland_dev -i ..\sahsuland_dev\rif40\functions\rif40_auto_indirect_checks.sql
+sqlcmd -d sahsuland_dev -i ..\sahsuland_dev\rif40\functions\rif40_db_name_check.sql
+sqlcmd -d sahsuland_dev -i ..\sahsuland_dev\rif40\functions\rif40_has_role.sql
+sqlcmd -d sahsuland_dev -i ..\sahsuland_dev\rif40\functions\rif40_is_object_resolvable.sql
+sqlcmd -d sahsuland_dev -i ..\sahsuland_dev\rif40\functions\rif40_num_denom_validate.sql
+sqlcmd -d sahsuland_dev -i ..\sahsuland_dev\rif40\functions\rif40_object_resolve.sql
+sqlcmd -d sahsuland_dev -i ..\sahsuland_dev\rif40\functions\rif40_sequence_current_value.sql
