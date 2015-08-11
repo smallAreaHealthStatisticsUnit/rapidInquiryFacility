35211bf06e3088cbf8d793444c2be89684f86ec5
Margaret Douglass <mdouglas@wpea-rif2.ic.ac.uk>
2015-08-11 18:17:59 +0100

    SQL Server import data
    Edited 2 more scripts

--- rifDatabase/SQLserver/installation/rif40_import_sahsuland.sql
+++ rifDatabase/SQLserver/installation/rif40_import_sahsuland.sql
@@ -1,9 +1,58 @@
-TRUNCATE TABLE [rif40].[rif40_version]
-GO
-
+/*
 BULK
 INSERT [rif40].[rif40_version]
 FROM '$(path)\Postgres\sahsuland\data\rif40_version.csv'
+WITH
+(
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '\n'
+)
+GO
+
+BULK
+INSERT [rif40].[t_rif40_projects]
+FROM '$(path)\Postgres\sahsuland\data\t_rif40_projects.csv'
+WITH
+(
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '\n'
+)
+GO
+
+BULK
+INSERT [rif_data].[sahsuland_level1]
+FROM '$(path)\Postgres\sahsuland\data\sahsuland_level1.csv'
+WITH
+(
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '\n'
+)
+GO
+
+BULK
+INSERT [rif_data].[sahsuland_level2]
+FROM '$(path)\Postgres\sahsuland\data\sahsuland_level2.csv'
+WITH
+(
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '\n'
+)
+GO
+*/
+
+BULK
+INSERT [rif_data].[sahsuland_level3]
+FROM '$(path)\Postgres\sahsuland\data\sahsuland_level3.csv'
+WITH
+(
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '\n'
+)
+GO
+
+BULK
+INSERT [rif_data].[sahsuland_level4]
+FROM '$(path)\Postgres\sahsuland\data\sahsuland_level4.csv'
 WITH
 (
 FIELDTERMINATOR = ',',
