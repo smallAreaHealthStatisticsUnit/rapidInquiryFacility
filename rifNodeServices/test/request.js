a1b5cbaf4f34fa0bef49b6c75a7a45e5b787252a
Peter Hambly <phambly@fastmail.co.uk>
2016-01-18 13:42:20 +0000

    Parameter passing

--- rifNodeServices/test/request.js
+++ rifNodeServices/test/request.js
@@ -10,18 +10,27 @@ var MakeRequest = function(){
     this.request = require('request');
 	var json_file = fs.createReadStream(inputFile);
     var length = fs.statSync(inputFile).size;
-	
-    var formData = {
-       my_field: 'my_value',
-//       my_buffer: new Buffer([1, 2, 3]),     
-       attachments: [
-         json_file
-      ]
-    };
-
-   console.log("Sending " + inputFile + " request:" + nRequests + "; length: " + length); 
-   
-     this.options = {
+	var id=nRequests;
+	var formData = {
+		my_test: "Defaults",
+		my_reference: nRequests,
+		attachments: [
+			json_file
+		]
+	};
+	if (nRequests == 2) {
+		formData["verbose"]="true";
+		formData["my_test"]="Verbose";		
+	}
+	else if (nRequests == 3) {
+		formData["verbose"]="true";
+		formData["zoomLevel"]=0;		
+		formData["my_test"]="Verbose";		
+	}	
+	console.log("Sending " + inputFile + " request:" + nRequests + "; length: " + length +
+		'; ' + JSON.stringify(formData, null, 4));
+		
+    this.options = {
         url:  'http://127.0.0.1:3000/toTopojson',
         headers:{'Content-Type': contentType},
         formData: formData, 
@@ -30,22 +39,24 @@ var MakeRequest = function(){
 }
 
 var postIt = function(){
-   var r = new MakeRequest();       
-   r.request.post(r.options, function optionalCallback(err, httpResponse, body) {
-   if (err) {
-      return console.error('Upload #' + nRequests + ' failed: ' + JSON.stringify(httpResponse, null, 4) + "\r\nError: ", err);
-   }
-   else if (httpResponse.statusCode != 200) {
-      return console.error('Upload #' + nRequests + ' failed HTTP status: ' + httpResponse.statusCode + "\r\nError: [", body + "]\r\n");
-	   
-   }
-   else if (nRequests == 1) {
-		console.log('Upload #' + nRequests + ': successful!  Server responded with:\r\n', body);
-    }
-	else {
-		console.log('Upload #' + nRequests + ': successful!');
-	}
- });
+	var r = new MakeRequest();       
+	var p=r.request.post(r.options, function optionalCallback(err, httpResponse, body) {
+		if (err) {
+			return console.error('Upload #' + nRequests + ' failed: ' + JSON.stringify(httpResponse, null, 4) + "\r\nError: ", err);
+		}
+		else if (httpResponse.statusCode != 200) {
+			return console.error('Upload #' + nRequests + ' failed HTTP status: ' + httpResponse.statusCode + "\r\nError: [", body + "]\r\n");
+		   
+		}
+			else {
+				var jsonData = JSON.parse(body);
+				var json = JSON.stringify(jsonData.topojson)
+				console.log('Upload #' + nRequests + ': '+ jsonData.message + '; new length: ' + json.length);
+			}
+		});
+//	var f=p.form();
+ 
+//	f.append('verbose', true);
 };
   
 
