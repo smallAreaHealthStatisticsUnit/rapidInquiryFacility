74fdcc4ecc149d74509d1ba8ad0fd655741abe85
dwmorley <dwmorley@ic.ac.uk>
2016-06-21 16:08:32 +0100

    Fred's 4 RIF css merged to one file
    merge fred's CSS files to one to avoid x4 duplication

--- rifWebApplication/src/main/webapp/WEB-INF/modules/rifm-app.js
+++ rifWebApplication/src/main/webapp/WEB-INF/modules/rifm-app.js
@@ -1,3 +1,43 @@
+/**
+ * <hr>
+ * The Rapid Inquiry Facility (RIF) is an automated tool devised by SAHSU 
+ * that rapidly addresses epidemiological and public health questions using 
+ * routinely collected health and population data and generates standardised 
+ * rates and relative risks for any given health outcome, for specified age 
+ * and year ranges, for any given geographical area.
+ *
+ * Copyright 2016 Imperial College London, developed by the Small Area
+ * Health Statistics Unit. The work of the Small Area Health Statistics Unit 
+ * is funded by the Public Health England as part of the MRC-PHE Centre for 
+ * Environment and Health. Funding for this project has also been received 
+ * from the United States Centers for Disease Control and Prevention.  
+ *
+ * <pre> 
+ * This file is part of the Rapid Inquiry Facility (RIF) project.
+ * RIF is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RIF is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with RIF. If not, see <http://www.gnu.org/licenses/>; or write 
+ * to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Boston, MA 02110-1301 USA
+ * </pre>
+ * <hr>
+ */
+
+/*
+ * MODULE for the main RIF web application
+ * Third-party modules injected here
+ * Navbar state changes and notifications handled
+ */
+
 angular.module("RIF",
         [
             "ui.router",
@@ -41,7 +81,7 @@ angular.module("RIF",
                 $urlRouterProvider.otherwise("/submission");  //login
             }])
         .controller('AlertCtrl', function ($scope, notifications) {
-            //CONTROLLER to handle alert bars and notofications
+            //CONTROLLER to handle alert bars and notifications
             //This is on the main <body> scope in index
             //Subsequent contollers in data-ui-view are children
             $scope.hide = false;
@@ -58,23 +98,3 @@ angular.module("RIF",
         .run(function ($rootScope, $state) {
             $rootScope.$state = $state;
         });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
