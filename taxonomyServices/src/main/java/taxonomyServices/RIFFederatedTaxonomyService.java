af868e2145072c64ea55d90d61c305787cd4440c
Kevin Garwood <kevin_garwood@hotmail.co.uk>
2016-07-07 10:24:58 +0100

    Initialising service with class file location

--- taxonomyServices/src/main/java/taxonomyServices/RIFFederatedTaxonomyService.java
+++ taxonomyServices/src/main/java/taxonomyServices/RIFFederatedTaxonomyService.java
@@ -1,33 +1,35 @@
 package taxonomyServices;
 
+
 import rifGenericLibrary.system.ClassFileLocator;
-import rifGenericLibrary.system.RIFServiceException;
 import rifGenericLibrary.taxonomyServices.*;
+
+import java.util.ArrayList;
 
 public class RIFFederatedTaxonomyService extends DefaultFederatedTaxonomyService {
 
-	
 	public static void main(String[] args) {
+		RIFFederatedTaxonomyService service
+			= new RIFFederatedTaxonomyService();
 		try {
-			RIFFederatedTaxonomyService service
-				= new RIFFederatedTaxonomyService();
-			service.initialiseService();	
+			String defaultResourceDirectoryPath
+				= ClassFileLocator.getClassRootLocation("taxonomyServices");
+			service.initialise(defaultResourceDirectoryPath);
+			
+			ArrayList<TaxonomyTerm> taxonomyTerms
+				= service.getRootTerms("icd10");
+			for (TaxonomyTerm taxonomyTerm : taxonomyTerms) {
+				System.out.println("TaxonomyTerm=="+taxonomyTerm.getLabel()+"==");
+				ArrayList<TaxonomyTerm> subTerms
+					= service.getImmediateChildTerms("icd10", taxonomyTerm.getIdentifier());
+				for (TaxonomyTerm subTerm : subTerms) {
+					System.out.println("CHILD TERM=="+subTerm.getLabel()+"==");
+				}
+			}
 		}
-		catch(RIFServiceException rifServiceException) {
-			rifServiceException.printErrors();
+		catch(Exception exception) {
+			exception.printStackTrace(System.out);
 		}
-	}
-	
-	public RIFFederatedTaxonomyService() {
-		
-	}
-	
-	public void initialiseService() 
-		throws RIFServiceException {
-
-		String defaultResourceDirectoryPath
-			= ClassFileLocator.getClassRootLocation("taxonomyServices");
-		initialise(defaultResourceDirectoryPath);		
 	}
 	
 }
