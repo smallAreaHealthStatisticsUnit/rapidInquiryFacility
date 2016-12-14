89d6e92db31f9f62b19bbb026d01d6dc2438fca6
Peter Hambly <phambly@fastmail.co.uk>
2016-12-14 08:14:01 +0000

    Improve sahsuland_empty target
    No need to rebuild other DBs

--- rifDatabase/Postgres/psql_scripts/recreate_sahsuland_empty_db.sql
+++ rifDatabase/Postgres/psql_scripts/recreate_sahsuland_empty_db.sql
@@ -61,8 +61,156 @@ BEGIN
 END;
 $$;
 
+\set ntestuser '''XXXX':testuser''''
+SET rif40.testuser TO :ntestuser;
+
 DROP DATABASE IF EXISTS sahsuland_empty;
 CREATE DATABASE sahsuland_empty WITH OWNER rif40 /* No sahsuland tablespace */;
 COMMENT ON DATABASE sahsuland_empty IS 'RIF V4.0 PostGres SAHSULAND Empty Database';
+
+\c sahsuland_empty postgres :pghost
+--
+-- Start transaction 3: sahsuland_empty build
+--
+BEGIN;
+--
+-- Check user is postgres on sahsuland_empty
+--
+\set ECHO NONE
+SET rif40.testuser TO :ntestuser;
+DO LANGUAGE plpgsql $$
+DECLARE	
+	c1 CURSOR(l_schema VARCHAR) FOR
+		SELECT * FROM information_schema.schemata
+		 WHERE LOWER(schema_name) = LOWER(l_schema);
+	c2 CURSOR FOR
+		 SELECT usename
+		   FROM pg_user
+		 WHERE pg_has_role(usename, 'rif_manager', 'MEMBER')
+		    OR pg_has_role(usename, 'rif_user', 'MEMBER')
+		    OR pg_has_role(usename, 'rif_student', 'MEMBER')
+		  ORDER BY 1;
+	c1_rec RECORD;
+	c2_rec RECORD;
+--
+	schemalist VARCHAR[]:=ARRAY['rif40_dmp_pkg', 'rif40_sql_pkg', 'rif40_sm_pkg', 'rif40_log_pkg', 'rif40_trg_pkg',
+			'rif40_geo_pkg', 'rif40_xml_pkg', 'rif40_R_pkg', 
+			'rif40', 'gis', 'pop', 'rif_studies', 'rif_data', 'data_load', 'rif40_partitions'];
+	x VARCHAR;
+	sql_stmt VARCHAR;
+	u_name	VARCHAR;
+BEGIN
+	u_name:=LOWER(SUBSTR(CURRENT_SETTING('rif40.testuser'), 5));
+	IF user = 'postgres' AND current_database() = 'sahsuland_empty' THEN
+		RAISE INFO 'db_create.sql() User check: %', user;	
+	ELSE
+		RAISE EXCEPTION 'db_create.sql() C209xx: User check failed: % is not postgres on sahsuland_empty database (%)', 
+			user, current_database();	
+	END IF;
+--
+	sql_stmt:='CREATE EXTENSION IF NOT EXISTS postgis';
+	RAISE INFO 'SQL> %;', sql_stmt::VARCHAR;
+	EXECUTE sql_stmt;
+	sql_stmt:='CREATE EXTENSION IF NOT EXISTS adminpack';
+	RAISE INFO 'SQL> %;', sql_stmt::VARCHAR;
+	EXECUTE sql_stmt;
+--
+-- RIF40 grants
+--	
+	sql_stmt:='GRANT ALL ON DATABASE sahsuland_empty to rif40';
+	RAISE INFO 'SQL> %;', sql_stmt::VARCHAR;
+	EXECUTE sql_stmt;
+	sql_stmt:='REVOKE CREATE ON SCHEMA public FROM rif40';
+	RAISE INFO 'SQL> %;', sql_stmt::VARCHAR;
+	EXECUTE sql_stmt;
+--
+	sql_stmt:='GRANT CONNECT ON DATABASE sahsuland_empty to '||u_name;
+	RAISE INFO 'SQL> %;', sql_stmt::VARCHAR;
+	EXECUTE sql_stmt;
+--
+-- Add user, rif_studies schema and PKG (package) schemas
+--
+	OPEN c1(u_name);
+	FETCH c1 INTO c1_rec;
+	CLOSE c1;
+	IF c1_rec.schema_name IS NULL THEN
+		sql_stmt:='CREATE SCHEMA '||u_name||' AUTHORIZATION '||u_name;
+		RAISE INFO 'SQL> %;', sql_stmt::VARCHAR;
+		EXECUTE sql_stmt;
+		sql_stmt:='GRANT ALL 
+		ON SCHEMA '||u_name||' TO '||u_name;
+		RAISE INFO 'SQL> %;', sql_stmt::VARCHAR;
+		EXECUTE sql_stmt;
+	END IF;
+--
+-- Re-create user schemas
+--
+	FOR c2_rec IN c2 LOOP
+		OPEN c1(c2_rec.usename);
+		FETCH c1 INTO c1_rec;
+		CLOSE c1;
+		IF c1_rec.schema_name IS NULL THEN
+			sql_stmt:='CREATE SCHEMA '||c2_rec.usename||' AUTHORIZATION '||c2_rec.usename;
+			RAISE INFO 'SQL> %;', sql_stmt::VARCHAR;
+			EXECUTE sql_stmt;
+			sql_stmt:='GRANT ALL 
+			ON SCHEMA '||c2_rec.usename||' TO '||c2_rec.usename;
+			RAISE INFO 'SQL> %;', sql_stmt::VARCHAR;
+			EXECUTE sql_stmt;
+		END IF;	
+	END LOOP;
+--
+	FOREACH x IN ARRAY schemalist LOOP
+		OPEN c1(x);
+		FETCH c1 INTO c1_rec;
+		CLOSE c1;
+		IF c1_rec.schema_name IS NULL THEN
+			sql_stmt:='CREATE SCHEMA '||x||' AUTHORIZATION rif40';
+			RAISE INFO 'SQL> %;', sql_stmt::VARCHAR;
+			EXECUTE sql_stmt;
+		END IF;
+		sql_stmt:='GRANT ALL ON SCHEMA '||x||' TO rif40 WITH GRANT OPTION';
+		RAISE INFO 'SQL> %;', sql_stmt::VARCHAR;
+		EXECUTE sql_stmt;
+		sql_stmt:='GRANT USAGE ON SCHEMA '||x||' TO rif_user';
+		RAISE INFO 'SQL> %;', sql_stmt::VARCHAR;
+		EXECUTE sql_stmt;
+		sql_stmt:='GRANT USAGE ON SCHEMA '||x||' TO rif_manager WITH GRANT OPTION';
+		RAISE INFO 'SQL> %;', sql_stmt::VARCHAR;
+		EXECUTE sql_stmt;
+--
+-- Grant CREATE privilege to data load schema; allow onwards grants
+--
+		IF x IN ('rif_data', 'data_load') THEN
+				sql_stmt:='GRANT CREATE ON SCHEMA '||x||' TO rif_manager WITH GRANT OPTION';
+				RAISE INFO 'SQL> %;', sql_stmt::VARCHAR;
+				EXECUTE sql_stmt;
+		END IF;
+	END LOOP;
+
+--
+-- Set default search pathname
+--
+	sql_stmt:='ALTER DATABASE sahsuland_empty SET search_path TO rif40, public, topology, gis, pop, rif_data, data_load, rif40_sql_pkg, rif_studies, rif40_partitions';
+	RAISE INFO 'SQL> %;', sql_stmt::VARCHAR;
+	EXECUTE sql_stmt;
+END;
+$$;
+\set echo ALL
+
+--
+-- Set search path for rif40
+--
+ALTER USER rif40 SET search_path 
+	TO rif40, public, topology, gis, pop, rif_data, data_load, rif40_sql_pkg, rif_studies, rif40_partitions;
+SET search_path 
+	TO rif40, public, topology, gis, pop, rif_data, data_load, rif40_sql_pkg, rif_studies, rif40_partitions;
+	
+--
+-- End transaction 3: sahsuland_empty build
+--
+END;
+
+
 --
 -- Eof
