e692b31a176086ea593f530d7e0713568b24cff5
Peter Hambly <phambly@fastmail.co.uk>
2017-01-23 17:18:01 +0000

    Remove v4_0_rif40_geo_pkg_simplification.sql

--- rifDatabase/Postgres/psql_scripts/v4_0_create_sahsuland_empty.sql
+++ rifDatabase/Postgres/psql_scripts/v4_0_create_sahsuland_empty.sql
@@ -209,7 +209,7 @@ $$;
 -- PG psql code (geo processing)
 --
 \i ../PLpgsql/v4_0_rif40_geo_pkg.sql
-\i ../PLpgsql/rif40_geo_pkg/v4_0_rif40_geo_pkg_simplification.sql
+--\i ../PLpgsql/rif40_geo_pkg/v4_0_rif40_geo_pkg_simplification.sql
 
 --
 -- PG psql code (middleware)
