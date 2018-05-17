64babf3239fd06e205135785202b279d7ffa8748
Martin McCallion <martin@devilgate.org>
2018-05-17 14:57:32 +0100

    Tweaks

--- rifWebApplication/src/main/webapp/WEB-INF/backend/services/rifs-back-requests.js
+++ rifWebApplication/src/main/webapp/WEB-INF/backend/services/rifs-back-requests.js
@@ -62,7 +62,7 @@ angular.module("RIF")
                     formData.append("fileField", blob, "submissionFile.txt");
                     formData.append("fileFormat", "JSON");
 
-                    return $http.post(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + "submitStudy/", formData, {
+                    return $http.post(servicesConfig.studySubmissionURL + "submitStudy/", formData, {
                         transformRequest: angular.identity,
                         headers: {'Content-Type': undefined}
                     });
@@ -75,18 +75,18 @@ angular.module("RIF")
                     //http://localhost:8080/rifServices/studySubmission/login?userID=kgarwood&password=xyz
                     //[{"result":"User kgarwood logged in."}]
                     self.currentUser = username;
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'login?userID=' + username + '&password=' + password);
+                    return $http.get(servicesConfig.studySubmissionURL + 'login?userID=' + username + '&password=' + password);
                 };
                 self.logout = function (username) {
                     //http://localhost:8080/rifServices/studySubmission/logout?userID=kgarwood
                     //[{"result":"User kgarwood logged out."}]
                     self.currentUser = "";
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'logout?userID=' + username);
+                    return $http.get(servicesConfig.studySubmissionURL + 'logout?userID=' + username);
                 };
                 self.isLoggedIn = function (username) {
                     //http://localhost:8080/rifServices/studySubmission/isLoggedIn?userID=kgarwood
                     //[{"result":"true"}]
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'isLoggedIn?userID=' + username);
+                    return $http.get(servicesConfig.studySubmissionURL + 'isLoggedIn?userID=' + username);
                 };
                 self.getDatabaseType = function (username) {
                     //http://localhost:8080/rifServices/studySubmission/ms/getDatabaseType?userID=peter
@@ -117,70 +117,70 @@ angular.module("RIF")
                 self.getProjects = function (username) {
                     //http://localhost:8080/rifServices/studySubmission/getProjects?userID=kgarwood
                     //[{"name":"TEST","description":null}]
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'getProjects?userID=' + username, config);
+                    return $http.get(servicesConfig.studySubmissionURL + 'getProjects?userID=' + username, config);
                 };
                 self.getProjectDescription = function (username, projectName) {
                     //http://localhost:8080/rifServices/studySubmission/getProjectDescription?userID=kgarwood&projectName=TEST
                     //[{"result":"Test Project. Will be disabled when in production."}]
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'getProjectDescription?userID=' + username + '&projectName=' + projectName, config);
+                    return $http.get(servicesConfig.studySubmissionURL + 'getProjectDescription?userID=' + username + '&projectName=' + projectName, config);
                 };
                 self.getGeographies = function (username) {
                     //http://localhost:8080/rifServices/studySubmission/getGeographies?userID=kgarwood
                     //[{"names":["EW01","SAHSU","UK91"]}]
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'getGeographies?userID=' + username, config);
+                    return $http.get(servicesConfig.studySubmissionURL + 'getGeographies?userID=' + username, config);
                 };
                 self.getHealthThemes = function (username, geography) {
                     //http://localhost:8080/rifServices/studySubmission/getHealthThemes?userID=kgarwood&geographyName=SAHSU
                     //[{"name":"SAHSULAND","description":"SAHSU land cancer incidence example data"}]
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'getHealthThemes?userID=' + username + '&geographyName=' + geography, config);
+                    return $http.get(servicesConfig.studySubmissionURL + 'getHealthThemes?userID=' + username + '&geographyName=' + geography, config);
                 };
                 self.getNumerator = function (username, geography, healthThemeDescription) {
                     //http://localhost:8080/rifServices/studySubmission/getNumerator?userID=kgarwood&geographyName=SAHSU&healthThemeDescription=SAHSU%20land%20cancer%20incidence%20example%20data
                     //[{"numeratorTableName":"SAHSULAND_CANCER","numeratorTableDescription":"Cancer cases in SAHSU land","denominatorTableName":"SAHSULAND_POP","denominatorTableDescription":"SAHSU land population"}]
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'getNumerator?userID=' + username + '&geographyName=' + geography + "&healthThemeDescription=" + healthThemeDescription, config);
+                    return $http.get(servicesConfig.studySubmissionURL + 'getNumerator?userID=' + username + '&geographyName=' + geography + "&healthThemeDescription=" + healthThemeDescription, config);
                 };
                 //Investigation parameters
                 self.getYearRange = function (username, geography, numeratorTableName) {
                     //http://localhost:8080/rifServices/studySubmission/getYearRange?userID=kgarwood&geographyName=SAHSU&numeratorTableName=SAHSULAND_CANCER
                     //[{"lowerBound":"1989","upperBound":"1996"}]
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'getYearRange?userID=' + username + '&geographyName=' + geography + '&numeratorTableName=' + numeratorTableName, config);
+                    return $http.get(servicesConfig.studySubmissionURL + 'getYearRange?userID=' + username + '&geographyName=' + geography + '&numeratorTableName=' + numeratorTableName, config);
                 };
                 self.getSexes = function (username) {
                     //http://localhost:8080/rifServices/studySubmission/getSexes?userID=kgarwood
                     //[{"names":["Males","Females","Both"]}]
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'getSexes?userID=' + username, config);
+                    return $http.get(servicesConfig.studySubmissionURL + 'getSexes?userID=' + username, config);
                 };
                 self.getCovariates = function (username, geography, geoLevel) {
                     //http://localhost:8080/rifServices/studySubmission/getCovariates?userID=kgarwood&geographyName=SAHSU&geoLevelToMapName=LEVEL4
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'getCovariates?userID=' + username + '&geographyName=' + geography + '&geoLevelToMapName=' + geoLevel, config);
+                    return $http.get(servicesConfig.studySubmissionURL + 'getCovariates?userID=' + username + '&geographyName=' + geography + '&geoLevelToMapName=' + geoLevel, config);
                 };
                 self.getAgeGroups = function (username, geography, numerator) {
                     //http://localhost:8080/rifServices/studySubmission/getAgeGroups?userID=kgarwood&geographyName=SAHSU&numeratorTableName=SAHSULAND_CANCER
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'getAgeGroups?userID=' + username + '&geographyName=' + geography + '&numeratorTableName=' + numerator, config);
+                    return $http.get(servicesConfig.studySubmissionURL + 'getAgeGroups?userID=' + username + '&geographyName=' + geography + '&numeratorTableName=' + numerator, config);
                 };
                 //geography info
                 self.getDefaultGeoLevelSelectValue = function (username, geography) {
                     //http://localhost:8080/rifServices/studySubmission/getDefaultGeoLevelSelectValue?userID=kgarwood&geographyName=SAHSU
                     //[{"names":["LEVEL2"]}]
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'getDefaultGeoLevelSelectValue?userID=' + username + '&geographyName=' + geography, config);
+                    return $http.get(servicesConfig.studySubmissionURL + 'getDefaultGeoLevelSelectValue?userID=' + username + '&geographyName=' + geography, config);
                 };
                 self.getGeoLevelViews = function (username, geography, geoLevelSelectName) {
                     //http://localhost:8080/rifServices/studySubmission/getGeoLevelViews?userID=kgarwood&geographyName=SAHSU&geoLevelSelectName=LEVEL2
                     //[{"names":["LEVEL2","LEVEL3","LEVEL4"]}]
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'getGeoLevelViews?userID=' + username + '&geographyName=' + geography +
+                    return $http.get(servicesConfig.studySubmissionURL + 'getGeoLevelViews?userID=' + username + '&geographyName=' + geography +
                             '&geoLevelSelectName=' + geoLevelSelectName, config);
                 };
                 self.getGeoLevelSelectValues = function (username, geography) {
                     //http://localhost:8080/rifServices/studySubmission/getGeoLevelSelectValues?userID=kgarwood&geographyName=SAHSU
                     //[{"names":["LEVEL1","LEVEL2","LEVEL3","LEVEL4"]}]
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'getGeoLevelSelectValues?userID=' + username + '&geographyName=' + geography, config);
+                    return $http.get(servicesConfig.studySubmissionURL + 'getGeoLevelSelectValues?userID=' + username + '&geographyName=' + geography, config);
                 };
                 //statistical methods
                 self.getAvailableCalculationMethods = function (username) {
                     //http://localhost:8080/rifServices/studySubmission/getAvailableCalculationMethods?userID=kgarwood
                     //[{"codeRoutineName":"car_r_procedure","prior":"Standard deviation","description":"Applies ...
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'getAvailableCalculationMethods?userID=' + username, config);
                 };
+                    return $http.get(servicesConfig.studySubmissionURL + 'getAvailableCalculationMethods?userID=' + username, config);
                 //status
                 self.getCurrentStatusAllStudies = function (username) {
                     //http://localhost:8080/rifServices/studyResultRetrieval/getCurrentStatusAllStudies?userID=kgarwood              
@@ -237,7 +237,7 @@ angular.module("RIF")
                 //get areas used in a completed study (recycled faulty KG method)
                 self.getStudySubmission = function (username, studyID) {
                     //http://localhost:8080/rifServices/studySubmission/getStudySubmission?userID=kgarwood&studyID=274
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'getStudySubmission?userID=' + username + '&studyID=' + studyID);
+                    return $http.get(servicesConfig.studySubmissionURL + 'getStudySubmission?userID=' + username + '&studyID=' + studyID);
                 };
                 //get details of a completed study
                 self.getDetailsForProcessedStudy = function (username, studyID) {
@@ -259,23 +259,23 @@ angular.module("RIF")
                 //Save study tables to Zip file
                 self.createZipFile = function (username, studyID, zoomLevel, getTimeout) {
                     //http://localhost:8080/rifServices/studySubmission/pg/createZipFile?userID=dwmorley&studyID=46
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'createZipFile?userID=' + username + '&studyID=' + studyID + "&zoomLevel=" + zoomLevel, 
+                    return $http.get(servicesConfig.studySubmissionURL + 'createZipFile?userID=' + username + '&studyID=' + studyID + "&zoomLevel=" + zoomLevel,
 						{timeout: getTimeout /* in mS: 360s */});
                 };                
 				//Fetch Zip file
                self.getZipFileURL = function (username, studyID, zoomLevel) {
                     //http://localhost:8080/rifServices/studySubmission/pg/getZipFile?userID=dwmorley&studyID=46
-                    return servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'getZipFile?userID=' + username + '&studyID=' + studyID + "&zoomLevel=" + zoomLevel;
+                    return servicesConfig.studySubmissionURL + 'getZipFile?userID=' + username + '&studyID=' + studyID + "&zoomLevel=" + zoomLevel;
                 };         
 				//Get Extract Status - can Zip file be created/fetched
                self.getExtractStatus = function (username, studyID) {
                     //http://localhost:8080/rifServices/studySubmission/pg/getExtractStatus?userID=dwmorley&studyID=46
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'getExtractStatus?userID=' + username + '&studyID=' + studyID);
+                    return $http.get(servicesConfig.studySubmissionURL 'getExtractStatus?userID=' + username + '&studyID=' + studyID);
                 };
 				// Get JSON study setup file
 				self.getJsonFile = function (username, studyID) {
                     //http://localhost:8080/rifServices/studySubmission/pg/getJsonFile?userID=dwmorley&studyID=46
-                    return $http.get(servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'getJsonFile?userID=' + username + '&studyID=' + studyID);
+                    return $http.get(servicesConfig.studySubmissionURL 'getJsonFile?userID=' + username + '&studyID=' + studyID);
                 };     
 				
 				//Middleware logger for front end
@@ -289,7 +289,7 @@ angular.module("RIF")
 					actualTime,
 					relativeTime) {
                     //http://localhost:8080/rifServices/studySubmission/pg/rifFrontEndLogger?userID=dwmorley&messageType=INFO&browserType=XXX&message=Hello
-					var uri=servicesConfig.studySubmissionURL + DatabaseService.getDatabase() + 'rifFrontEndLogger?userID=' + username + 
+					var uri=servicesConfig.studySubmissionURL 'rifFrontEndLogger?userID=' + username +
 						'&messageType=' + messageType + 
 						'&browserType=' + browserType + 
 						'&message=' + encodeURIComponent((message || "NO MESSAGE!"));
