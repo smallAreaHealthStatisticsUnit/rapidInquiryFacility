16a03179f74396ab58711c48d0b40f6d7b83098f
Peter Hambly <phambly@fastmail.co.uk>
2018-06-08 15:40:20 +0100

    Fix div by 0 error

--- rifNodeServices/sql/postgres/area_centroid_report.sql
+++ rifNodeServices/sql/postgres/area_centroid_report.sql
@@ -8,24 +8,26 @@
  * Description:			Area and centroids report
  * Note:				%% becomes % after substitution
  */
-WITH a AS (
+ WITH a AS (
 	SELECT areaname,
-		   ROUND(area_km2::numeric, 2) AS area_km2,
-		   ROUND(
-				(ST_Area(geography(%1))/(1000*1000))::numeric, 2) AS area_km2_calc,
+		   ROUND(area_km2::numeric, 4) AS area_km2,
+		   ROUND((ST_Area(geography(%1))/(1000*1000))::numeric, 4) AS area_km2_calc,
 		   ROUND(ST_X(geographic_centroid)::numeric, 4)||','||ROUND(ST_Y(geographic_centroid)::numeric, 4) AS geographic_centroid,
 		   ROUND(ST_X(ST_Centroid(%1))::numeric, 4)||','||ROUND(ST_Y(ST_Centroid(%1))::numeric, 4) AS geographic_centroid_calc,
-		   ROUND(ST_Distance_Sphere(ST_Centroid(%1), geographic_centroid)::numeric/1000, 2) AS centroid_diff_km
+		   ROUND(ST_DistanceSphere(ST_Centroid(%1), geographic_centroid)::numeric/1000, 2) AS centroid_diff_km
 	  FROM %2
 	 GROUP BY areaname, area_km2, %1, geographic_centroid
 )
 SELECT a.areaname,
        a.area_km2,
 	   a.area_km2_calc,
-	   ROUND(100*(ABS(a.area_km2 - a.area_km2_calc)/area_km2_calc), 2) AS pct_km2_diff,
+	   CASE 
+			WHEN area_km2_calc = 0 THEN 0 
+			ELSE ROUND(100*(ABS(a.area_km2 - a.area_km2_calc)/area_km2_calc), 4) 
+	   END AS pct_km2_diff,
 	   a.geographic_centroid,
-      a.geographic_centroid_calc,
+       a.geographic_centroid_calc,
 	   a.centroid_diff_km
   FROM a
  ORDER BY 1
- LIMIT 100
+ LIMIT 100;
