f250c352ab16859f1522d595d5bafad003d79c0c
Martin McCallion <martin@devilgate.org>
2018-07-27 16:21:58 +0100

    Forgot to add this file before: RiskAnalysisStudyContentHandler.java

---
+++ rifServices/src/main/java/org/sahsu/rif/services/fileformats/RiskAnalysisStudyContentHandler.java
@@ -0,0 +1,10 @@
+package org.sahsu.rif.services.fileformats;
+
+public class RiskAnalysisStudyContentHandler extends AbstractStudyContentHandler {
+
+	public RiskAnalysisStudyContentHandler() {
+
+		super();
+		setSingularRecordName("risk_analysis_study");
+	}
+}
