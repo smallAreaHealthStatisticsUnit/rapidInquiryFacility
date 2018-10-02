5da42375a52384a9056ef9e3c25a23fe5082b83c
Martin McCallion <martin@devilgate.org>
2018-10-02 12:56:07 +0100

    Correct and simplify the copying of the rerun files.

--- rifServices/src/main/R/CreateWindowsScript.R
+++ rifServices/src/main/R/CreateWindowsScript.R
@@ -2,101 +2,15 @@
 # Objective : Generate the Windows version of the script used for rerunning studies.
 # Created by: Martin McCallion
 # Created on: 21/09/2018
+
 createWindowsScript <- function(script_name) {
 
 	if (exists("catalina_home")) {
-		performSmoothingActivityScriptA <- file.path(catalina_home, "webapps", "rifServices", 
-		"WEB-INF", "classes", script_name) # Source
-		performSmoothingActivityScriptB <- file.path(scratchSpace, "performSmoothingActivity.R") # Target
-		if (!file.exists(performSmoothingActivityScriptB)) {
-			cat(paste("Copy: ", performSmoothingActivityScriptA, " to: ", 
-				performSmoothingActivityScriptB, "\n"), sep="")
 
-			tryCatch({
-				file.copy(performSmoothingActivityScriptA, performSmoothingActivityScriptB)
-			},
-				warning = function(w) {
-					cat(paste("UNABLE to copy: ", performSmoothingActivityScriptA, " to: ",
-						performSmoothingActivityScriptB, w, "\n"), sep="")
-					exitValue <<- 0
-			},
-				error = function(e) {
-					cat(paste("ERROR copying: ", performSmoothingActivityScriptA, " to: ",
-						performSmoothingActivityScriptB, e, "\n"), sep="")
-					exitValue <<- 1
-			}) # End of tryCatch
-		} else {
-			cat(paste("WARNING! No need to copy: ", performSmoothingActivityScriptA, " to: ",
-				performSmoothingActivityScriptB, "\n"), sep="")
-		}
-
-		Adj_Cov_Smooth_csvA <- file.path(catalina_home, "webapps", "rifServices", "WEB-INF", 
-			"classes", "Adj_Cov_Smooth_csv.R") # Source
-		Adj_Cov_Smooth_csvB <- file.path(scratchSpace, "Adj_Cov_Smooth_csv.R") # Target
-		if (!file.exists(Adj_Cov_Smooth_csvB)) {
-			cat(paste("Copy: ", Adj_Cov_Smooth_csvA, " to: ", Adj_Cov_Smooth_csvB, "\n"), sep="")
-
-			tryCatch({
-				file.copy(Adj_Cov_Smooth_csvA, Adj_Cov_Smooth_csvB)
-			},
-				warning=function(w) {
-				cat(paste("UNABLE to copy: ", Adj_Cov_Smooth_csvA, " to: ", Adj_Cov_Smooth_csvB, w, "\n"), sep="")
-				exitValue <<- 0
-			},
-				error=function(e) {
-				cat(paste("ERROR copying: ", Adj_Cov_Smooth_csvA, " to: ", Adj_Cov_Smooth_csvB, e, 
-					"\n"), sep="")
-				exitValue <<- 1
-			}) # End of tryCatch
-		} else {
-			cat(paste("WARNING! No need to copy: ", Adj_Cov_Smooth_csvA, " to: ", 
-				Adj_Cov_Smooth_csvB, "\n"), sep="")
-		}
-
-		Adj_Cov_Smooth_CommonA <- file.path(catalina_home, "webapps", "rifServices", "WEB-INF",
-		"classes", "Adj_Cov_Smooth_Common.R") # Source
-		Adj_Cov_Smooth_CommonB <- file.path(scratchSpace, "Adj_Cov_Smooth_Common.R") # Target
-		if (!file.exists(Adj_Cov_Smooth_CommonB)) {
-			cat(paste("Copy: ", Adj_Cov_Smooth_CommonA, " to: ", Adj_Cov_Smooth_CommonB, "\n"), sep="")
-
-			tryCatch({
-				file.copy(Adj_Cov_Smooth_CommonA, Adj_Cov_Smooth_CommonB)
-			},
-			warning=function(w) {
-				cat(paste("UNABLE to copy: ", Adj_Cov_Smooth_CommonA, " to: ", Adj_Cov_Smooth_CommonB, w, "\n"), sep="")
-				exitValue <<- 0
-			},
-			error=function(e) {
-				cat(paste("ERROR copying: ", Adj_Cov_Smooth_CommonA, " to: ", Adj_Cov_Smooth_CommonB, e,
-				"\n"), sep="")
-				exitValue <<- 1
-			}) # End of tryCatch
-		} else {
-			cat(paste("WARNING! No need to copy: ", Adj_Cov_Smooth_CommonA, " to: ",
-			Adj_Cov_Smooth_CommonB, "\n"), sep="")
-		}
-
-		rif40_run_RA <- file.path(catalina_home, "webapps", "rifServices", "WEB-INF", "classes", 
-			"rif40_run_R.bat") # Source
-		rif40_run_RB <- file.path(scratchSpace, "rif40_run_R.bat") # Target
-		
-		if (!file.exists(rif40_run_RB)) {
-			cat(paste("Copy: ", rif40_run_RA, " to: ", rif40_run_RB, "\n"), sep="")
-
-			tryCatch({
-				file.copy(rif40_run_RA, rif40_run_RB)
-			},
-				warning=function(w) {
-				cat(paste("UNABLE to copy: ", rif40_run_RA, " to: ", rif40_run_RB, w, "\n"), sep="")
-				exitValue <<- 0
-			},
-				error=function(e) {
-				cat(paste("ERROR copying: ", rif40_run_RA, " to: ", rif40_run_RB, e, "\n"), sep="")
-				exitValue <<- 1
-			}) # End of tryCatch
-		} else {
-			cat(paste("WARNING! No need to copy: ", rif40_run_RA, " to: ", rif40_run_RB, "\n"), sep="")
-		}
+		copy_file(script_name)
+		copy_file("performSmoothingActivity.R")
+		copy_file("Adj_Cov_Smooth_Common.R")
+		copy_file("rif40_run_R.bat")
 
 		#
 		# Create rif40_run_R_env.bat
@@ -133,4 +47,28 @@ createWindowsScript <- function(script_name) {
 			exitValue <<- 1
 		}) # End of tryCatch
 	}
+}
+
+copy_file <- function(script_name) {
+
+	source <- file.path(catalina_home, "webapps", "rifServices", "WEB-INF", "classes", script_name)
+	target <- file.path(scratchSpace, script_name)
+	if (!file.exists(target)) {
+		cat(paste("Copy: ", source, " to: ", target, "\n"), sep="")
+
+		tryCatch({
+			file.copy(source, target)
+		},
+		warning = function(w) {
+			cat(paste("UNABLE to copy: ", source, " to: ", target, w, "\n"), sep="")
+			exitValue <<- 0
+		},
+		error = function(e) {
+			cat(paste("ERROR copying: ", source, " to: ", target, e, "\n"), sep="")
+			exitValue <<- 1
+		}) # End of tryCatch
+	} else {
+		cat(paste("WARNING! No need to copy: ", source, " to: ", target, "\n"), sep="")
+	}
+
 }
