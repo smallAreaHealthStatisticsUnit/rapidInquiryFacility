98c15164449258df8c48d386d65e12bbeb242033
Martin McCallion <martin@devilgate.org>
2018-10-09 15:02:03 +0100

    Add skeleton ICD 9 class.

---
+++ taxonomyServices/src/main/java/org/sahsu/taxonomyservices/Icd9TaxonomyService.java
@@ -0,0 +1,15 @@
+package org.sahsu.taxonomyservices;
+
+import org.sahsu.rif.generic.system.RIFServiceException;
+import org.sahsu.rif.generic.taxonomyservices.AbstractTaxonomyService;
+import org.sahsu.rif.generic.taxonomyservices.TaxonomyServiceConfiguration;
+
+public class Icd9TaxonomyService extends AbstractTaxonomyService {
+
+	@Override
+	public void initialiseService(final String defaultResourceDirectoryPath,
+			final TaxonomyServiceConfiguration taxonomyServiceConfiguration)
+			throws RIFServiceException {
+
+	}
+}
