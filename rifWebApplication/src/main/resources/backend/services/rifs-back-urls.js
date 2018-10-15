7a92b8279394a55a2405e79ce7628deab90863cc
Martin McCallion <martin@devilgate.org>
2018-10-15 17:35:45 +0100

    Rename taxonomy to taxonomies in the URL.

--- rifWebApplication/src/main/resources/backend/services/rifs-back-urls.js
+++ rifWebApplication/src/main/resources/backend/services/rifs-back-urls.js
@@ -44,7 +44,7 @@ angular.module("RIF")
 				return {		 
 					studySubmissionURL: serviceHost + "/rifServices/studySubmission/",
 					studyResultRetrievalURL: serviceHost + "/rifServices/studyResultRetrieval/",
-					taxonomyServicesURL: serviceHost + "/taxonomy/service/"
+					taxonomyServicesURL: serviceHost + "/taxonomies/service/"
 				}
 /* 
 
--- taxonomyServices/src/main/java/org/sahsu/taxonomyservices/RIFTaxonomyWebServiceApplication.java
+++ taxonomyServices/src/main/java/org/sahsu/taxonomyservices/RIFTaxonomyWebServiceApplication.java
@@ -9,7 +9,7 @@ import javax.ws.rs.core.Application;
 import org.sahsu.rif.generic.util.TaxonomyLogger;
 
 /**
- * The root of the Taxonomy Service web service. The root path, taxonomy,
+ * The root of the Taxonomy Service web service. The root path, taxonomies,
  * is provided by the name of the application's war file.
  */
 @ApplicationPath("/service")
