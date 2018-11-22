0d4bc6dfc5d70bcadf09f73c69c7302470db13db
Peter Hambly <phambly@fastmail.co.uk>
2018-11-22 17:07:05 +0000

    Make TaxonomyLogger use common RIFLogger code

--- rifGenericLibrary/src/main/java/org/sahsu/rif/generic/util/TaxonomyLogger.java
+++ rifGenericLibrary/src/main/java/org/sahsu/rif/generic/util/TaxonomyLogger.java
@@ -18,8 +18,9 @@ import java.util.Arrays;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.sahsu.rif.generic.util.RIFLogger;
 
-public final class TaxonomyLogger {
+public final class TaxonomyLogger extends RIFLogger {
 
 	//TOUR_CONCURRENCY
 	/*
@@ -57,8 +58,10 @@ public final class TaxonomyLogger {
 			setManager=true;
 		}
 		
-		try {
-			log=LogManager.getLogger(TaxonomyLogger.class.getName());	
+		Logger log = null;
+		try {	
+			log = LogManager.getLogger(TaxonomyLogger.class.getName());
+			setLog(log);
 		} catch(Exception e) {
 			System.out.println("TaxonomyLogger() LogManager.getLogger: Caught exception: "
 			                   + e.getMessage());
@@ -78,109 +81,5 @@ public final class TaxonomyLogger {
 	public static TaxonomyLogger getLogger() { // Return this static object
 		return taxonomyLogger;
 	}	
-	
-	/**
-	 * error() print error message and stack trace. Contains message, root cause message, throwable count,
-	 * All root cause stack traces.
-	 * <p>
-	 * May NOT be used in statically defined functions (because of this.getClass()). 
-	 *
-	 * @param  callingClass  Calling class (object); usually this.getClass()
-	 * @param  errorHeading  Textual error heading
-	 * @param  throwableItem Exception caught
-	 */
-	public void error(
-		final Class callingClass,
-		final String errorHeading,
-		final Throwable throwableItem) {
-			
-		StringBuilder errorString = new StringBuilder();
-		
-		errorString.append("getMessage:          ").append(ExceptionUtils.getMessage(
-				throwableItem)).append(lineSeparator);
-        errorString.append("getRootCauseMessage: ")
-		        .append(ExceptionUtils.getRootCauseMessage(throwableItem)).append(lineSeparator);
-        errorString.append("getThrowableCount:   ")
-		        .append(ExceptionUtils.getThrowableCount(throwableItem)).append(lineSeparator);
-		errorString.append("getRootCauseStackTrace >>>").append(lineSeparator);
-		for (String cause : ExceptionUtils.getRootCauseStackTrace(throwableItem)) {
-			errorString.append(cause).append(lineSeparator);
-		}
-		errorString.append("<<< End getRootCauseStackTrace.");
-		
-		if (log != null) {
-			log.error("["+callingClass.getName()+"]:" + lineSeparator + 
-				errorHeading + lineSeparator + errorString);
-		} else {
-			System.out.println("ERROR(no RIFLogger):" + lineSeparator + "["
-			                   + callingClass.getName() + "]" + errorHeading + lineSeparator
-			                   + Arrays.toString(throwableItem.getStackTrace()));
-		}
-	}
-	
-	// error() do not print stack trace
-	public void error(
-		final Class callingClass,
-		final String errorHeading) {		
-		
-		if (log != null) {
-			log.error("["+callingClass.getName()+"]:" + lineSeparator + 
-				errorHeading);
-		}
-		else {	
-			System.out.println("ERROR(no TaxonomyLogger):" + lineSeparator + "["+callingClass.getName()+"]" + 
-				errorHeading);
-		}
-	}
 
-		// warning() print stack tracr
-	public void warning(
-		final Class callingClass,
-		final String errorHeading,
-		final Throwable throwableItem) {
-			
-		StringBuilder errorString = new StringBuilder();
-		
-		errorString.append("getMessage:          " + ExceptionUtils.getMessage(throwableItem) + lineSeparator);
-        errorString.append("getRootCauseMessage: " + ExceptionUtils.getRootCauseMessage(throwableItem) + lineSeparator);
-        errorString.append("getThrowableCount:   " + ExceptionUtils.getThrowableCount(throwableItem) + lineSeparator);
-		errorString.append("getRootCauseStackTrace >>>" + lineSeparator);
-		for (String cause : ExceptionUtils.getRootCauseStackTrace(throwableItem)) {
-			errorString.append(cause + lineSeparator);
-		}
-		errorString.append("<<< End getRootCauseStackTrace.");
-		
-		if (log != null) {
-			log.warn("["+callingClass.getName()+"]:" + lineSeparator + 
-				errorHeading + lineSeparator + errorString);
-		}
-		else {	
-			System.out.println("WARNING(no TaxonomyLogger):" + lineSeparator + "["+callingClass.getName()+"]" + 
-				errorHeading + lineSeparator + throwableItem.getStackTrace());
-		}
-	}
-		
-	public void warning(
-		final Class callingClass,			
-		final String message) {
-		
-		if (log != null) {
-			log.warn("["+callingClass.getName()+"]:" + lineSeparator + message);
-		}
-		else {	
-			System.out.println("WARNING(no TaxonomyLogger):" + lineSeparator + "["+callingClass.getName()+"]" + message);
-		}
-	}
-	
-	public void info(
-		final Class callingClass,			
-		final String message) {
-		
-		log.info("["+callingClass.getName()+"]:" + lineSeparator + message);
-	}
-
-	public void debug(final Class callingClass, final String message) {
-
-		log.debug("[" + callingClass.getName() + "]:" + lineSeparator + message);
-	}
 }
