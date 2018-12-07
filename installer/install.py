bf87e7db86e2eb0f546b59cff559684315b40d0e
Martin McCallion <martin@devilgate.org>
2018-12-07 17:42:43 +0000

    Add the start of having a settings file.

--- installer/install.py
+++ installer/install.py
@@ -11,17 +11,35 @@ __email__ = "m.mccallion@imperial.ac.uk"
 import os
 import sys
 from collections import namedtuple
+from pathlib import Path
 import shutil
 import argparse
 import configparser
 
+db_type = ""
+script_home = ""
+tomcat_home = ""
+war_files_location = ""
+development: False
+
+all_settings = {"development": "Development mode?",
+                "db_type": "Database type",
+                "script_home": "Directory for scripts",
+                "tomcat_home": "Home directory for Tomcat",
+                "war_files_location": "Directory containing the WAR files",
+                }
 
 def main():
 
     # Check that we have all the values we need
     # db_type, script_root, cat_home, war_dir = check_arguments()
 
-    args = check_arguments()
+    # args = check_arguments()
+
+    setttings = get_settings()
+
+
+
     print("About to install with the following settings:"
           "\n\tDB: {} "
           "\n\tScripts directory: {} "
@@ -47,6 +65,46 @@ def main():
         # shutil.copy(f, cat_home + "/webapps")
 
 # enddef main()
+
+
+def get_settings():
+    """Prompt the user for the installation settings.
+
+    Gets the current values from ~/.rif/rifInstall.ini, if that
+    file exists. Writes back to the same file (replacing it) after the
+    user has confirmed. If the file does not exist, we load the defaults from
+    install.ini in the current directory.
+    """
+
+    # Create the RIF home directory and properties file if they don't exist
+    home_dir = Path.home()
+    rif_home = home_dir / ".rif"
+    rif_home.mkdir(parents=True, exist_ok=True)
+    user_props = rif_home / "rifInstall.ini"
+    user_props.touch(exist_ok=True)
+    default_props = Path.cwd() / ".rif" / "install.ini"
+
+    # We have the default file in the current directory and the user's
+    # version in their home.
+    default_config = configparser.ConfigParser(allow_no_value=True)
+    default_config.read(default_props)
+    user_config = configparser.ConfigParser(allow_no_value=True)
+    user_config.read(user_props)
+
+    for key, prompt in all_settings.items():
+        if key in user_config:
+            setting = user_config[key]
+        elif key in default_config:
+            setting = default_config[key]
+
+        # Get the value from the user
+        value = input(prompt)
+
+        # Update the user's config value
+        user_config.set(key, value)
+
+    # Update the user's config file
+    user_config.write()
 
 
 def long_db_name(db):
