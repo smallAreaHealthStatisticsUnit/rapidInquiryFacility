f1b35eaebd4f848590ecf335368e072aecf8fd43
Martin McCallion <martin@devilgate.org>
2019-02-04 15:52:26 +0000

    Condition some recent scripts on Postgres being at version >= 10, since I have 9.x running on Windows VM.

--- rifDatabase/Postgres/PLpgsql/rif40_trg_pkg/trigger_fct_rif40_tables_checks.sql
+++ rifDatabase/Postgres/PLpgsql/rif40_trg_pkg/trigger_fct_rif40_tables_checks.sql
@@ -55,14 +55,44 @@
 \timing
 
 --
+-- Only do this if Postgres version is 10 or above
+--
+
+DO LANGUAGE plpgsql $$
+DECLARE
+	c1 CURSOR FOR
+	 	SELECT version() AS version,
+	 	current_setting('server_version_num')::NUMERIC as numeric_version;
+	c1_rec RECORD;
+--
+BEGIN
+	BEGIN
+		OPEN c1;
+		FETCH c1 INTO c1_rec;
+		CLOSE c1;
+	EXCEPTION
+		WHEN others THEN
+            RAISE WARNING 'db_create.sql(): unsupported version() function: %', version();
+            RAISE;
+	END;
+	--
+	IF c1_rec.numeric_version < 10000 THEN
+		RAISE INFO 'Not running trigger_fct_rif40_tables_checks.sql on Postgres version %',
+			c1_rec.version::VARCHAR;
+		RETURN;
+	END IF;
+END;
+$$;
+
+--
 -- Check user is rif40
 --
 DO LANGUAGE plpgsql $$
 BEGIN
 	IF user = 'rif40' THEN
-		RAISE INFO 'User check: %', user;	
+		RAISE INFO 'User check: %', user;
 	ELSE
-		RAISE EXCEPTION 'C20900: User check failed: % is not rif40', user;	
+		RAISE EXCEPTION 'C20900: User check failed: % is not rif40', user;
 	END IF;
 END;
 $$;
@@ -99,14 +129,14 @@ Check table_name, total_field, sex_field_name, age_group_field_name, age_sex_gro
 			 WHERE c.oid        = b.attrelid
 			   AND c.relname    = a.tablename
 			   AND c.relkind    = 'r' /* Relational table */
-			   AND c.relpersistence IN ('p', 'u') /* Persistence: permanent/unlogged */ 
-			 UNION		 
+			   AND c.relpersistence IN ('p', 'u') /* Persistence: permanent/unlogged */
+			 UNION
 			SELECT UPPER(c.relname) AS tablename, UPPER(b.attname) AS columnname, UPPER(n.nspname) AS schemaname	/* Tables */
 			  FROM pg_partitioned_table p, pg_attribute b, pg_class c, pg_namespace n
 			 WHERE p.partrelid    = c.oid
 			   AND c.oid          = b.attrelid
 			   AND c.relnamespace = n.oid
-			 UNION			 
+			 UNION
 			SELECT UPPER(a.viewname) AS tablename, UPPER(b.attname) AS columnname, UPPER(schemaname) AS schemaname	/* Views */
 			  FROM pg_views a, pg_attribute b, pg_class c
 			 WHERE c.oid        = b.attrelid
@@ -115,7 +145,7 @@ Check table_name, total_field, sex_field_name, age_group_field_name, age_sex_gro
 			 UNION
 			SELECT UPPER(a.relname) AS tablename, UPPER(d.attname) AS columnname, UPPER(n.nspname) AS schemaname				/* User FDW foreign tables */
 			  FROM pg_foreign_table b, pg_roles r, pg_attribute d, pg_class a
-				LEFT OUTER JOIN pg_namespace n ON (n.oid = a.relnamespace)			
+				LEFT OUTER JOIN pg_namespace n ON (n.oid = a.relnamespace)
 			 WHERE b.ftrelid  = a.oid
 			   AND a.relowner = (SELECT oid FROM pg_roles WHERE rolname = USER)
 			   AND a.relowner = r.oid
@@ -138,7 +168,7 @@ BEGIN
 	IF TG_OP = 'DELETE' THEN
 		RETURN OLD;
 	END IF;
-     
+
 	schema:=rif40_sql_pkg.rif40_object_resolve(NEW.table_name::VARCHAR);
 --
 -- Normally the schema owner will not be able to see the data tables, so there is no error here
@@ -203,9 +233,9 @@ BEGIN
 	IF (NEW.age_sex_group_field_name IS NOT NULL AND NEW.age_sex_group_field_name::text <> '') THEN
 		PERFORM rif40_trg_pkg.rif40_db_name_check('AGE_SEX_GROUP_FIELD_NAME', NEW.age_sex_group_field_name);
 	END IF;
---  	
+--
 	RETURN NEW;
-END; 
+END;
 $BODY$
 LANGUAGE 'plpgsql';
 COMMENT ON FUNCTION rif40_trg_pkg.trigger_fct_rif40_tables_checks() IS 'Check TABLE_NAME exists. DO NOT RAISE AN ERROR IF IT DOES; otherwise check, column <TABLE_NAME>.TOTAL_FIELD,  column <TABLE_NAME>.ICD_FIELD_NAME exists. This allows the RIF40 schema owner to not have access to the tables. Access is checked in RIF40_NUM_DENOM. Automatic (Able to be used in automatic RIF40_NUM_DENOM (0/1, default 0). A user specific T_RIF40_NUM_DENOM is supplied for other combinations. Cannot be applied to direct standardisation denominator) is restricted to 1 denominator per geography.
@@ -217,12 +247,12 @@ CREATE TRIGGER rif40_tables_checks
 	BEFORE INSERT OR UPDATE OF isdirectdenominator, table_name, total_field,
 		sex_field_name, age_group_field_name, age_sex_group_field_name ON rif40_tables
 	FOR EACH ROW
-	WHEN ((NEW.table_name IS NOT NULL AND NEW.table_name::text <> '') OR 
-		(NEW.isdirectdenominator IS NOT NULL AND NEW.isdirectdenominator::text <> '') OR 
-		(NEW.table_name IS NOT NULL AND NEW.table_name::text <> '') OR 
+	WHEN ((NEW.table_name IS NOT NULL AND NEW.table_name::text <> '') OR
+		(NEW.isdirectdenominator IS NOT NULL AND NEW.isdirectdenominator::text <> '') OR
+		(NEW.table_name IS NOT NULL AND NEW.table_name::text <> '') OR
 		(NEW.total_field IS NOT NULL AND NEW.total_field::text <> '') OR
-		(NEW.sex_field_name IS NOT NULL AND NEW.sex_field_name::text <> '') OR 
-		(NEW.age_group_field_name IS NOT NULL AND NEW.age_group_field_name::text <> '') OR 
+		(NEW.sex_field_name IS NOT NULL AND NEW.sex_field_name::text <> '') OR
+		(NEW.age_group_field_name IS NOT NULL AND NEW.age_group_field_name::text <> '') OR
 		(NEW.age_sex_group_field_name IS NOT NULL AND NEW.age_sex_group_field_name::text <> ''))
 	EXECUTE PROCEDURE rif40_trg_pkg.trigger_fct_rif40_tables_checks();
 COMMENT ON TRIGGER rif40_tables_checks ON rif40_tables IS 'INSERT OR UPDATE trigger: calls rif40_trg_pkg.trigger_fct_rif40_tables_checks()';
