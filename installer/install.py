874406347d03ecda09d980f6cc20cf67d98c6f86
Martin McCallion <martin@devilgate.org>
2019-02-26 16:57:12 +0000

    Improve install prompting and Postgres password handling.

--- installer/install.py
+++ installer/install.py
@@ -12,6 +12,7 @@ __email__ = "m.mccallion@imperial.ac.uk"
 import hashlib
 import os
 import platform
+import psycopg2
 import re
 import shutil
 import subprocess
@@ -37,18 +38,24 @@ RIF40_PASSWORD = "db_rif40_password"
 POSTGRES_PASSWORD = "db_pg_password"
 
 prompt_strings = {DEVELOPMENT_MODE: "Development mode?",
-                  DB_TYPE: "Database type (pg or ms)",
+                  DB_TYPE: "Database type (pg or ms for PostgreSQL or MS "
+                           "SQL Server)",
                   SCRIPT_HOME: "Directory for SQL scripts",
                   TOMCAT_HOME: "Home directory for Tomcat",
                   WAR_FILES_LOCATION: "Directory containing the WAR files",
-                  EXTRACT_DIRECTORY: "Directory for files extracted by studies",
-                  DATABASE_NAME: "Name of the new database (default: "
-                                 "sahsuland)",
-                  DATABASE_USER: "User name for the new database (and the "
-                                 "RIF)",
-                  DATABASE_PASSWORD: "Password for the new user",
-                  RIF40_PASSWORD: "Password for the 'rif40' user",
-                  POSTGRES_PASSWORD: "Password for the 'postgres' user"
+                  EXTRACT_DIRECTORY: "Please specify a directory where files "
+                                     "extracted by studies should be created",
+                  DATABASE_NAME: "What do you want to call the new database? "
+                                 "(the default is 'sahsuland')",
+                  DATABASE_USER: "We will create a new user in the {} "
+                                 "database, which will also be your RIF user "
+                                 "name; what do you want to call it?",
+                  DATABASE_PASSWORD: "Please set a password for the '{}' user",
+                  RIF40_PASSWORD: "Please set a password for the 'rif40' "
+                                  "user, which we will also create",
+                  POSTGRES_PASSWORD: "Please give the password for the "
+                                     "'postgres' user, which is the "
+                                     "administrator of the Postgres system"
                   }
 
 # We have the default settings file in the current directory and the user's
@@ -56,10 +63,10 @@ prompt_strings = {DEVELOPMENT_MODE: "Development mode?",
 # settings, and the database-specific ones and the [NOPROMPT] one.
 default_parser = ConfigParser(allow_no_value=True,
                               interpolation=ExtendedInterpolation())
-default_parser.optionxform = str # Preserve case in keys
+default_parser.optionxform = str  # Preserve case in keys
 user_parser = ConfigParser(allow_no_value=True,
                            interpolation=ExtendedInterpolation())
-user_parser.optionxform = str # Preserve case in keys
+user_parser.optionxform = str  # Preserve case in keys
 default_config = ConfigParser()
 user_config = ConfigParser()
 
@@ -68,9 +75,8 @@ user_props = Path()
 
 
 def main():
-
     banner("WARNING: This will DELETE any existing database of the "
-           "name specified (default: Sahsuland).\n\n Proceed with caution.",
+           "name specified (default: sahsuland).\n\n Proceed with caution.",
            60)
     if not go("Continue? [Default: No]: "):
         return
@@ -89,13 +95,13 @@ def main():
               "\n\tScripts directory: {} "
               "\n\tTomcat home directory: {}"
               "\n\tWAR files directory: {}"
-              "\n\tExtract directory: {}"
-                .format(bool(settings.dev_mode),
-                        long_db_name(settings.db_type),
-                        settings.script_root,
-                        settings.cat_home,
-                        settings.war_dir,
-                        settings.extract_dir))
+              "\n\tExtract directory: {}".format(
+            bool(settings.dev_mode),
+            long_db_name(settings.db_type),
+            settings.script_root,
+            settings.cat_home,
+            settings.war_dir,
+            settings.extract_dir))
 
         if go("Continue? [No]: "):
 
@@ -118,7 +124,7 @@ def main():
                 print("About to run {}; switching to {}".format(script,
                                                                 parent))
                 result = subprocess.run(script.split(), cwd=parent,
-                                        stderr=subprocess.STDOUT)
+                                        capture_output=True)
 
                 if result.returncode is not None and result.returncode != 0:
                     db_created = False
@@ -136,6 +142,8 @@ def main():
                 db_created = True
 
             if db_created:
+                # Write out the log of the database creation
+
                 # Deploy WAR files
                 for f in get_war_files(settings):
                     shutil.copy(f, settings.cat_home / "webapps")
@@ -217,7 +225,7 @@ def get_settings():
         settings.script_root = base_path
     else:
         settings.script_root = Path(get_value_from_user(SCRIPT_HOME,
-                                                  is_path=True)).resolve()
+                                                        is_path=True)).resolve()
 
     # Tomcat home: if it's not set we use the environment variable
     settings.cat_home = get_value_from_user(TOMCAT_HOME, is_path=True)
@@ -234,13 +242,22 @@ def get_settings():
     # For now the next few are only for Postgres
     if settings.db_type == "pg":
         settings.db_name = get_value_from_user(DATABASE_NAME).strip()
-        settings.db_user = get_value_from_user(DATABASE_USER).strip()
-        settings.db_pass = get_password_from_user(DATABASE_PASSWORD).strip()
+        settings.db_user = get_value_from_user(DATABASE_USER,
+                                               extra=settings.db_name).strip()
+        settings.db_pass = get_password_from_user(DATABASE_PASSWORD, extra=
+        settings.db_user).strip()
         settings.db_owner_name = "rif40"
         settings.db_owner_pass = get_password_from_user(RIF40_PASSWORD).strip()
         settings.db_superuser_name = "postgres"
-        settings.db_superuser_pass = get_password_from_user(POSTGRES_PASSWORD,
-                                               confirm=False).strip()
+        settings.db_superuser_pass = get_password_from_user(
+            POSTGRES_PASSWORD,
+            confirm=False).strip()
+        while not check_db_user(settings.db_superuser_name,
+                                settings.db_superuser_pass):
+            print("Incorrect password. Please try again.")
+            settings.db_superuser_pass = get_password_from_user(
+                POSTGRES_PASSWORD,
+                confirm=False).strip()
 
     # Update the user's config file
     # user_config["key"] = "reply"
@@ -248,17 +265,49 @@ def get_settings():
     props_file = open(user_props, "w")
     user_parser.write(props_file)
 
-
     print("Settings: {}".format(settings))
 
     return settings
 
 
-def get_value_from_user(key, is_path=False):
+def check_db_user(user, password):
+    """Check that the user and password work for the database."""
+    # Copied from https://pynative.com/python-postgresql-tutorial/
+
+    success = False
+    connection = None
+    cursor= None
+    try:
+        connection = psycopg2.connect(user=user,
+                                      password=password,
+                                      host="localhost",
+                                      port="5432",
+                                      database="postgres")
+        cursor = connection.cursor()
+        # Print PostgreSQL Connection properties
+        print(connection.get_dsn_parameters(), "\n")
+        # Print PostgreSQL version
+        cursor.execute("SELECT version();")
+        record = cursor.fetchone()
+        success = True
+        print("You are connected to - ", record, "\n")
+    except Exception as error:
+        print("Error while connecting to PostgreSQL", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+    return success
+
+
+def get_value_from_user(key, is_path=False, extra=""):
     """Get a new value from the user, prompting with the current value
        from the config files if one exists.
        :param key: the setting being processed
        :param is_path: whether or not the setting is a path-like object
+       :param extra: optional string value to be inserted into the prompt
+       string
     """
 
     current_value = ""
@@ -266,7 +315,11 @@ def get_value_from_user(key, is_path=False):
         current_value = user_config[key]
     elif key in default_config:
         current_value = default_config[key]
-    reply = input("{} [{}] ".format(prompt_strings.get(key), current_value))
+
+    base_prompt = prompt_strings.get(key)
+    prompt = "{} [{}]{} ".format(base_prompt.format(extra), current_value,
+                                 "" if base_prompt.endswith("?") else ":")
+    reply = input(prompt)
     if reply is None or reply.strip() == "":
         reply = current_value
 
@@ -281,7 +334,8 @@ def get_value_from_user(key, is_path=False):
                 print("CATALINA_HOME is not set in the environment and no "
                       "value given for {}."
                       .format(prompt_strings.get(TOMCAT_HOME)))
-                reply = input("{} [{}] ".format(prompt_strings.get(key), current_value))
+                reply = input(
+                    "{} [{}] ".format(prompt_strings.get(key), current_value))
             else:
                 reply = tomcat_home_str
 
@@ -299,7 +353,7 @@ def get_value_from_user(key, is_path=False):
     return returned_reply
 
 
-def get_password_from_user(key, confirm=True):
+def get_password_from_user(key, confirm=True, extra=""):
     """Get a password from the user, with suitable prompting, hidden input,
        and the standard confirmation dialogue.
     """
@@ -308,7 +362,7 @@ def get_password_from_user(key, confirm=True):
     p2 = "y"
     while p1.strip() != p2.strip():
         print()
-        p1 = getpass(prompt_strings.get(key))
+        p1 = getpass(prompt_strings.get(key).format(extra))
         if not confirm:
             return p1
         p2 = getpass("Confirm password")
@@ -316,6 +370,7 @@ def get_password_from_user(key, confirm=True):
             print()
             print("Passwords do not match")
     return p1
+
 
 def get_war_files(settings):
     """Return a list of the WAR files to deploy"""
@@ -374,12 +429,10 @@ def create_properties_file(settings):
 
 
 def long_db_name(db):
-
     return "Microsoft SQL Server" if db.strip() == "ms" else "PostgreSQL"
 
 
 def short_db_name(db):
-
     return "MSSQL" if db.strip() == "ms" else "POSTGRES"
 
 
@@ -485,10 +538,10 @@ def get_pg_scripts(settings):
     return [(s, script_root) for s in [main_script, sahsuland_script,
                                        dump_script, restore_script,
                                        alter1_script, alter2_script,
-                                       #alter3_script,
-                                       #alter4_script,
+                                       # alter3_script,
+                                       # alter4_script,
                                        alter5_script,
-                                       #alter6_script,
+                                       # alter6_script,
                                        alter7_script, alter8_script,
                                        alter9_script, alter10_script,
                                        alter11_script]
@@ -529,10 +582,19 @@ def encrypt_password(user, pwd):
 def save_pg_passwords(settings):
     """Save the captured passwords to the .pgpass or pgpass.conf file."""
 
-    if platform.system() == "Windows":
-        pass_file = Path(os.environ["APPDATA"]) / "postgresql" / "pgpass.conf"
+    # The standard location can be overridden by an environment variable.
+    # It can also be overridden by a parameter passed to pg_ctl, but working
+    # out whether that has been set is out of scope for now (because it
+    # would be too hard).
+    password_file_name = os.getenv("PGPASSFILE", None)
+    if password_file_name:
+        pass_file = Path(password_file_name)
     else:
-        pass_file = Path().home() / ".pgpass"
+        if platform.system() == "Windows":
+            pass_file = Path(
+                os.environ["APPDATA"]) / "postgresql" / "pgpass.conf"
+        else:
+            pass_file = Path().home() / ".pgpass"
 
     line = "{}:{}:{}:{}:{}\n"
 
@@ -554,7 +616,7 @@ def save_pg_passwords(settings):
         pass_file_lines.append(line.format("localhost", "5432",
                                            settings.db_name,
                                            settings.db_user,
-                                           settings.db_user_pass))
+                                           settings.db_pass))
     pass_file_content = "".join(pass_file_lines)
 
     # If the password file doesn't exist we just create it. But if it does,
@@ -582,7 +644,8 @@ def create_backup_file(file):
 
     while True:
         backup_file_name = "{}.{}.bak".format(file.name,
-                                           time.strftime("%Y-%m-%d-%H-%M-%S"))
+                                              time.strftime(
+                                                  "%Y-%m-%d-%H-%M-%S"))
         backup_file = Path(file.parent) / backup_file_name
         if not backup_file.exists():
             return backup_file
@@ -629,7 +692,8 @@ def set_special_db_permissions():
     geo_path = base_path / "GeospatialData" / "tileMaker"
     data_loader_path = base_path / "DataLoaderData" / "SAHSULAND"
     files_to_permit = [f for f in geo_path.glob("mssql_*") if f.is_file()]
-    files_to_permit.extend(f for f in data_loader_path.iterdir() if f.is_file())
+    files_to_permit.extend(
+        f for f in data_loader_path.iterdir() if f.is_file())
     files_to_permit.append(backup_path)
     for f in files_to_permit:
         print("Granting Windows permissions for {}".format(f))
@@ -658,14 +722,15 @@ def set_windows_permissions(file_name):
     entries[0]['AccessPermissions'] = ntsecuritycon.GENERIC_ALL
     entries[0]['Trustee']['Identifier'] = "Everyone"
 
-    sd = win32security.GetNamedSecurityInfo(file_name, win32security.SE_FILE_OBJECT,
-            win32security.DACL_SECURITY_INFORMATION)
+    sd = win32security.GetNamedSecurityInfo(file_name,
+                                            win32security.SE_FILE_OBJECT,
+                                            win32security.DACL_SECURITY_INFORMATION)
     dacl = sd.GetSecurityDescriptorDacl()
     dacl.SetEntriesInAcl(entries)
     win32security.SetNamedSecurityInfo(file_name, win32security.SE_FILE_OBJECT,
-        win32security.DACL_SECURITY_INFORMATION |
-        win32security.UNPROTECTED_DACL_SECURITY_INFORMATION,
-        None, None, dacl, None)
+                                       win32security.DACL_SECURITY_INFORMATION |
+                                       win32security.UNPROTECTED_DACL_SECURITY_INFORMATION,
+                                       None, None, dacl, None)
 
 
 def normalise_path_separators(p):
@@ -769,6 +834,7 @@ class Settings():
 
 class Logger(object):
     """Lumberjack class - duplicates sys.stdout to a log file and it's okay."""
+
     # I got this from https://stackoverflow.com/a/24583265/1517620
 
     def __init__(self, filename="install.log", mode="ab", buff=0):
